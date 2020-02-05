64314128c157c6c204d57541a43092ff6858dcd6
Chris Kalfas <christopherkalfas22@gmail.com>
2020-02-05 14:30:34 -0500

    Laid out basic components. Added Gif, GifList comps as wells as a no gif found comp. Lastly, created a basic search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import SearchForm from './components/SearchForm';
+import GifList from './components/GifList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  constructor() {
+    super();
+  } 
+
+  render() { 
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
