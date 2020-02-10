f52c6fb126bc6b7f2d85417b5cfeaecc30e8f1f0
Chris Kalfas <christopherkalfas22@gmail.com>
2020-02-10 17:18:45 -0500

    Built out and wired up Search Feature using Giphy API search endpoint and creating a template literal to dynamically change the value of each search query. Then to finish the Search App, I used a React reference to get direct access to DOM elements, incase I want to build this app out futher. Then I added a component rendering NO search results, if the get requests data arrays length is less than zero. Next, added default params so the app renders with fun gifs initially. Lastly, used conditional rendering to add a loading sign to users while app searches for results.

--- src/App.js
+++ src/App.js
@@ -9,22 +9,43 @@ export default class App extends Component {
   constructor() {
     super()
     this.state ={
-      gifs: []
+      gifs: [],
+      loading: true
     }
   }
-  
-  componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y')
+
+  componentDidMount(){
+    this.performSearch()
+  }
+
+  //Refactor2-Wired up search bar to take dynamic user input to render results
+  performSearch = (query = 'cats') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y`)
       .then(response => {
         this.setState({
-          gifs: response.data.data
+          gifs: response.data.data,
+          loading: false
         })
       })
       .catch(error => {
         console.log("Error fetching and parsing data",error)
       })
   }
+  
+  //Reactor1- used axios get method instead of fetch
+  // componentDidMount() {
+  //   axios.get('http://api.giphy.com/v1/gifs/trending?api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y')
+  //     .then(response => {
+  //       this.setState({
+  //         gifs: response.data.data
+  //       })
+  //     })
+  //     .catch(error => {
+  //       console.log("Error fetching and parsing data",error)
+  //     })
+  // }
 
+  // Initial solution-Using fetch request
   // componentDidMount() {
   //   fetch('http://api.giphy.com/v1/gifs/trending?api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y')
   //     .then(response => response.json())
@@ -43,11 +64,15 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch}/>      
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs}/>
+          {
+            (this.state.loading)
+            ? <p>Loading...</p>
+            : <GifList data={this.state.gifs} />
+          }
         </div>
       </div>
     );
