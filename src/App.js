c31abe148a279bf70f748385cc0881269e31ea08
Chris Kalfas <christopherkalfas22@gmail.com>
2020-02-10 16:15:58 -0500

    Refactored fetch requests to use Axios instead when the App component mounts. Axios is a promised based library similar to Fetch API, but it has stronger browser support and other built in features Im excited to check out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 import SearchForm from './components/SearchForm';
 import GifList from './components/GifList';
 
@@ -10,18 +11,30 @@ export default class App extends Component {
     this.state ={
       gifs: []
     }
-  } 
-
+  }
+  
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({gifs: responseData.data})
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y')
+      .then(response => {
+        this.setState({
+          gifs: response.data.data
+        })
       })
       .catch(error => {
-        console.log("Error fetching and parsing data", error)
+        console.log("Error fetching and parsing data",error)
       })
   }
+
+  // componentDidMount() {
+  //   fetch('http://api.giphy.com/v1/gifs/trending?api_key=5mscSow7ozMpCy6J76LJFTZF9dW1L53Y')
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       this.setState({ gifs: responseData.data })
+  //     })
+  //     .catch(error => {
+  //       console.log("Error fetching and parsing data", error)
+  //     })
+  // }
 
   render() { 
     console.log(this.state.gifs)
